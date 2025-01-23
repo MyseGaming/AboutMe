9467f75e06d6b7f2f639fedd7d542f7e7790ff84
Morten Diestelhorst <morten.diestelhorst@outlook.de>
2025-01-23 16:39:10 +0100

    Fix Slideshow

--- src/app/pages/components/slide-show/slide-show.component.ts
+++ src/app/pages/components/slide-show/slide-show.component.ts
@@ -12,25 +12,47 @@ import {BehaviorSubject, mergeMap, of} from 'rxjs';
   styleUrl: './slide-show.component.less'
 })
 export class SlideShowComponent implements AfterContentInit {
-  $currentPage: BehaviorSubject<number>;
+  currentPage$: BehaviorSubject<number>;
+  currentPage: number = 0;
 
   @ContentChildren(SlideComponent)
   slides!: QueryList<SlideComponent>;
 
   constructor() {
-    this.$currentPage = new BehaviorSubject(0); // Standardmäßig erste Seite anzeigen
+    this.currentPage$ = new BehaviorSubject(this.currentPage); // Standardmäßig erste Seite anzeigen
   }
 
   ngAfterContentInit() {
     this.slides.toArray().forEach((page: SlideComponent, index: number) => {
+      page.pageIndex = index;
+      page.currentPage = index === this.currentPage;
+      page.shrinkLeft = index === this.currentPage + 1;
       page.subscribeToDisplay(
-        this.$currentPage.pipe(mergeMap(currentPage => of(currentPage === index)))
+        this.currentPage$.pipe(mergeMap(currentPage => {
+          console.log('' + index + ': ' + Math.abs(index - currentPage));
+          return of(Math.abs(index - currentPage) <= 1)
+        }))
       );
     });
-    this.$currentPage.next(0);
+    this.currentPage$.next(this.currentPage);
   }
 
   showPage(index: number) {
-    this.$currentPage.next(index);
+    const slides = this.slides.toArray();
+
+    if (index < this.currentPage) {
+      // Show Lefthand Slide
+      slides[index].slideRight(true)
+      slides[this.currentPage].slideRight(false);
+    }
+
+    if (index > this.currentPage) {
+      // Show Righthand slide
+      slides[index].slideLeft(true);
+      slides[this.currentPage].slideLeft(false);
+    }
+
+    this.currentPage = index;
+    this.currentPage$.next(this.currentPage);
   }
 }
--- src/app/pages/components/slide-show/slide/slide.component.ts
+++ src/app/pages/components/slide-show/slide/slide.component.ts
@@ -8,15 +8,57 @@ import {Observable, of} from 'rxjs';
   styleUrl: './slide.component.less'
 })
 export class SlideComponent {
+  @HostBinding("style.--page-index") public pageIndex: number = 0;
+  @HostBinding('class.displayed') public displayed: boolean = true;
+  @HostBinding('class.grow-left') public growLeft: boolean = false;
+  @HostBinding('class.grow-right') public growRight: boolean = false;
+  @HostBinding('class.shrink-left') public shrinkLeft: boolean = false;
+  @HostBinding('class.shrink-right') public shrinkRight: boolean = false;
+  @HostBinding('class.current-page') public currentPage: boolean = false;
 
-  @HostBinding('class.hidden') public isHidden: boolean = true;
 
   public displayed$: Observable<boolean> = of(false);
 
   public subscribeToDisplay(display$: Observable<boolean>) {
     this.displayed$ = display$;
     this.displayed$.subscribe(displayed => {
-      this.isHidden = !displayed;
+      this.displayed = displayed;
     })
+  }
+
+  public slideLeft(slideIn: boolean) {
+    if (slideIn) {
+      this.growRight = true;
+      this.currentPage = true;
+
+      this.shrinkRight = false;
+      this.shrinkLeft = false;
+      this.growLeft = false;
+    } else {
+      this.shrinkLeft = true;
+
+      this.currentPage = false;
+      this.growLeft = false;
+      this.growRight = false;
+      this.shrinkRight = false;
+    }
+  }
+
+  public slideRight(slideIn: boolean) {
+    if (slideIn) {
+      this.growLeft = true;
+      this.currentPage = true;
+
+      this.shrinkLeft = false;
+      this.shrinkRight = false;
+      this.growRight = false;
+    } else {
+      this.shrinkRight = true;
+
+      this.currentPage = false;
+      this.growLeft = false;
+      this.growRight = false;
+      this.shrinkLeft = false;
+    }
   }
 }
